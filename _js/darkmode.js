b9c4947e5e85d7e1b029fee24b0244c3e084e2b7
Dale <dalemartyn@gmail.com>
2017-03-14 20:49:52 +0000

    change heading weight

--- _js/darkmode.js
+++ _js/darkmode.js
@@ -16,7 +16,7 @@
       }
     },
     create: function() {
-      var css = 'body{background-color:#263238;color:#fff}figcaption{background:#37474f;color:rgba(255,255,255,0.7)}code{background:#37474f}blockquote{border-color:rgba(255,255,255,0.7)}.highlight{background:#37474f;color:#eceff1}.post-meta{color:rgba(255,255,255,0.7)}.site-title-link{color:#fff}.site-nav a{color:rgba(255,255,255,0.7)}.post-link{color:rgba(255,255,255,0.7)}.star-logo__circle{fill:rgba(255,255,255,0.5)}.star-logo__star{fill:#263238}';
+      var css = 'body{background-color:#212b31;color:#fff}figcaption{background:#2b3940;color:rgba(255,255,255,0.5)}code{background:#2b3940}blockquote{border-color:rgba(255,255,255,0.4)}.highlight{background:#2b3940;color:#eceff1}.post-meta{color:rgba(255,255,255,0.5)}.site-title-link{color:#fff}.site-nav a{color:rgba(255,255,255,0.7)}.post-link{color:rgba(255,255,255,0.5)}.star-logo__circle{fill:rgba(255,255,255,0.4)}.star-logo__star{fill:#212b31}';
       var ss = document.createElement('style');
       ss.appendChild(document.createTextNode(css));
       this.ss = ss;
