b3ab1f1683ab16c010f9a2b9c8ac8da3c647380e
Dale <dalemartyn@gmail.com>
2019-10-14 21:14:55 +0100

    remove gulp-concat

--- gulp-tasks/dark-theme.js
+++ gulp-tasks/dark-theme.js
@@ -1,11 +1,9 @@
 const { src, dest } = require('gulp');
-const concat = require('gulp-concat');
 const uglify = require('gulp-uglify');
 
 
 function build() {
   return src('_js/darkmode.js')
-    .pipe(concat('dark.js'))
     .pipe(uglify())
     .pipe(dest('./_includes/js/'));
 }
